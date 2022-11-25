c853b16b94dc729a211dc77095e73410e4eed11c
shuang768 <113189677+shuang768@users.noreply.github.com>
2022-11-25 15:45:36 +0000

    done

--- Q1pythagoras.py
+++ Q1pythagoras.py
@@ -1,6 +1,8 @@
 import math
-integer = 0
-while True:
+import random
+def Q1():
+  
+  while True:
     try:
       integer=float(input("what is length of a\n"))
       intege=float(input("what is the length of b\n"))
@@ -19,3 +21,75 @@ while True:
         c=round(math.sqrt(c2),2)
         print("the hypotenuse of",integer,"and",intege,"is",c)
         break
+def Q2():
+  
+  integer = 0
+  while True:
+    try:
+      integer=float(input("what is the min value\n"))
+      intege=float(input("what is the max value\n"))
+        
+    except ValueError:
+        print("Please enter a number")
+        continue
+      
+    else:
+        print(random.randint(integer,intege))
+        break
+def Q3():
+  
+  a=random.randint(1,100)
+  b=random.randint(1,100)
+  c=a+b
+  integer = 0
+  while True:
+    try:
+      print(a,"+",b)
+      integer=int(input("what is the answer\n"))
+        
+    except ValueError:
+        print("Please enter a number")
+        continue
+      
+    else:
+        if integer==c:
+          print("correct")
+        else: 
+          print("wrong answer, the answer is ",c)
+        break
+
+
+task=0
+while True:
+    try:
+      print("which function do you want")
+      print("1- Q1pythagoras")
+      print("2- Q2randomrange")
+      print("3- Q3addition")
+      print("4- Q4picture")
+      print("0- exit")
+      task=int(input("enter your function "))
+
+    except ValueError:
+        print("Please enter a number")
+        continue
+      
+    else:
+      if task==1:
+        Q1()
+        break
+      elif task==2:
+        Q2()
+        break
+      elif task==3:
+         Q3()
+         break
+      elif task==4:
+          print("please check the other link")
+          break
+      elif task==0:
+          print("exit")
+          break
+      else: 
+        print("please enter a valid number")
+        continue 
